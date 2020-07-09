fccea9d8096e16b1fd5a2ae22893977538787e68
tatsuya828828 <kkjy011894@gmail.com>
2020-07-09 17:19:50 +0900

    [finish]03-05

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,7 @@
-import java.util.Random;
-
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("あなたの運勢を占います");
-		int fortune = new Random().nextInt(3);
-		++fortune;
-		switch(fortune) {
-			case 1:
-				System.out.println("大吉");
-				break;
-			case 2:
-				System.out.println("中吉");
-				break;
-			case 3:
-				System.out.println("吉");
-				break;
-			default:
-				System.out.println("凶");
-				break;
+		for(int i=0; i<10; i++) {
+			System.out.println((i+1) +"回目のこんにちは");
 		}
 	}
 }
