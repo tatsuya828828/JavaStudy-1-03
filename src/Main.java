5cac33cc13dd84559f0f11885d49ed0a397d77ea
tatsuya828828 <kkjy011894@gmail.com>
2020-07-09 17:31:06 +0900

    [finish]03-06

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++) {
-			System.out.println((i+1) +"回目のこんにちは");
+		for(int i=1; i<10; i++) {
+			for(int j=1; j<10; j++) {
+				System.out.print(i*j);
+				System.out.print(" ");
+			}
+			System.out.println("");
 		}
 	}
 }
